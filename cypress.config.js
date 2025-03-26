c80873acd7200d72a448e5882892a175c6ae4fc3
IvailoAleksandrov <ivailoaleksandrov123@gmail.com>
2025-03-26 11:55:55 +0200

    Update and fix config.js and package files

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,9 @@
-const { defineConfig } = require("cypress");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor", createEsbuildPlugin(config));
-      return config;
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
-    specPattern: "**/*.feature", // Allow Cypress to find .feature files
-    baseUrl: "https://yourapp.com",
   },
 });
